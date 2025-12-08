8dfe96e26cb016721d42b89ec9b977fb5e9c7087
wangxingkang <wang_xingkang@qq.com>
2025-12-08 18:20:10 +0800

    chore: lock deps

--- manifest.config.ts
+++ manifest.config.ts
@@ -1,7 +1,7 @@
 import { defineManifestConfig } from '@uni-helper/vite-plugin-uni-manifest'
 
 export default defineManifestConfig({
-  'name': 'vitesse-uni-app',
+  'name': 'uni-template',
   'appid': '',
   'description': '',
   'versionName': '1.0.0',
