70879c6a56ed7f28e3b5b829e95eb338792b52c0
wangxingkang <wang_xingkang@qq.com>
2025-12-08 23:42:33 +0800

    chore: style

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
-import { createSSRApp } from 'vue'
-import App from './App.vue'
-import store from './store'
-import 'uno.css'
+import { createSSRApp } from 'vue';
+import App from './App.vue';
+import store from './store';
+import 'uno.css';
 
 export function createApp() {
-  const app = createSSRApp(App)
-  app.use(store)
+  const app = createSSRApp(App);
+  app.use(store);
 
   return {
     app,
-  }
+  };
 }
