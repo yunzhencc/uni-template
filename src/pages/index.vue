8eae89f78c438fa7c9bb8f574ed657e41367699f
wangxingkang <wang_xingkang@qq.com>
2025-12-09 11:15:06 +0800

    feat: add example

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,19 @@
 <script setup lang="ts">
+import { loginApi } from '@/apis/auth';
+
 definePage({
   type: 'home',
-})
+});
+
+async function handleLogin() {
+  const result = await loginApi({ email: '1', password: '10' });
+  // eslint-disable-next-line no-console
+  console.log('result', result.token);
+}
+
+onMounted(() => {
+  handleLogin();
+});
 </script>
 
 <template>
